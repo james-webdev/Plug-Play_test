4111a9db49fef69f5e56c44a69865ee7931433dd
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-07-01 13:20:59 +0100

    background image tweaks

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import DownArrow from "../svgs/Down";
 const Main = () => {
   return (
     <div className="ml-5 mr-4 sm:mr-0 sm:ml-20">
-      <div className="text-white text-5xl 2xl:text-6xl mt-40 extralight minWidth max-w-2xl">
+      <div className="text-white text-5xl 2xl:text-6xl mt-40 extralight minWidth max-w-2xl landscapeMain">
         <p>Managing your health has never been easier.</p>
       </div>
       <div className="flex items-center text-white text-xs mt-8">
