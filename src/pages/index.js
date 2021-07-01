5579be87b7e478fbf77f88e021ba11c64a01979a
James Roe <58564719+james-webdev@users.noreply.github.com>
2021-07-01 14:00:48 +0100

    background image tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,7 @@ import BottomButton from "../components/BottomButton";
 const IndexPage = () => {
   return (
     <>
-      {/* <div className="">
-        <Layout>
-           <div className="border border-black ml-10 mainHeight mr-10">
-            <Main />
-          </div> 
-        </Layout>
-         <div className="mt-40 flex justify-center items-center bg-white w-full h-40 smallScreens">
-        <button className="w-2/3">
-          <p className="text-white rounded-full py-3 px-6 w-full buttonColor p-4">
-            Find a specialist
-          </p>
-        </button>
-      </div> 
-      </div> */}
-       <div className="bgimage -mb-24 sm:-mb-5 z-0 relative">
+      <div className="bgimage -mb-24 sm:-mb-5 z-0 relative">
         <div className="absolute">
           <Layout>
             <div className="mt-64 sm:mt-48">
@@ -28,10 +14,10 @@ const IndexPage = () => {
             </div>
           </Layout>
         </div>
-      </div> 
-       <div className="z-50">
+      </div>
+      <div className="z-50">
         <BottomButton />
-      </div>   
+      </div>
     </>
   );
 };
